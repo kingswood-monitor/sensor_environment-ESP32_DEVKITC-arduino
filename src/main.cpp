ac707b26ef168ed396857cff18f8954cbeac6400
Richard Lyon <richardlyon@fastmail.com>
2020-01-13 21:14:28 +0000

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,182 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <Preferences.h>
+
+#include "CompositeSensor.h"
+#include "sensor-utils.h"
+
+#include "secrets.h"
+#include "settings.h"
+
+/*************************** Settings *******************************************************
+ *  Configuration settings stored in permanent FLASH memory                            
+ */
+Preferences preferences;
+String device_type;
+int client_id;
+
+#define WRITE_SETTINGS false
+#define DEVICE_TYPE "ESP32_DEVKITC"
+#define CLIENT_ID 5 // See Location Scheme for location mapping
+/*******************************************************************************************/
+
+#define FIRMWARE_NAME "Environment Sensor"
+#define FIRMWARE_SLUG "sensor_environment-ESP32_DEVKITC-arduino"
+#define FIRMWARE_VERSION "0.2"
+#define REFRESH_MILLIS 3000
+
+String device_id;
+String make_device_id(String device_type, int id);
+
+// WiFi / MQTT configuration
+IPAddress mqtt_server(192, 168, 1, 30);
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+
+// Initialise sensors
+CompositeSensor mySensor;
+
+void setup_wifi();
+void reconnect();
+void updateReadings();
+void publish_float(int client, char *topic, float val);
+void publish_int(int client, char *topic, int val);
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(2000);
+
+  //preferences
+  preferences.begin("sensor", false);
+
+  if (WRITE_SETTINGS)
+  {
+    preferences.putString("device_type", DEVICE_TYPE);
+    preferences.putInt("client_id", CLIENT_ID);
+  }
+  else
+  {
+    device_type = preferences.getString("device_type", DEVICE_TYPE);
+    client_id = preferences.getInt("client_id", -1);
+  }
+
+  device_id = make_device_id(device_type, client_id);
+  utils::printBanner(FIRMWARE_NAME, FIRMWARE_SLUG, FIRMWARE_VERSION, device_id.c_str());
+
+  setup_wifi();
+  mqttClient.setServer(mqtt_server, 1883);
+
+  mySensor.begin();
+}
+
+unsigned int cnt = 0;
+bool state = true;
+void loop()
+{
+  if (!mqttClient.connected())
+    reconnect();
+
+  mqttClient.loop();
+
+  updateReadings();
+
+  delay(3000);
+}
+
+/** Callback funciton for main loop to update and publish readings 
+*/
+void updateReadings()
+{
+  CompositeSensor::SensorReadings readings = mySensor.readSensors();
+
+  publish_float(client_id, "data/temperature", readings.temp);
+  publish_int(client_id, "data/humidity", readings.humidity);
+  publish_int(client_id, "data/co2", readings.co2);
+
+  char buf[100];
+  sprintf(buf, "T:%.1f, H:%.0f%, CO2:%d", readings.temp, readings.humidity, readings.co2);
+  Serial.println(buf);
+}
+
+String make_device_id(String device_type, int id)
+{
+  return device_type + "-" + id;
+}
+
+/************** WiFi / MQTT  *******************************************************************/
+
+void setup_wifi()
+{
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(SSID);
+
+  WiFi.begin(SSID, PASSWD);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect()
+{
+  // Loop until we're reconnected
+  while (!mqttClient.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (mqttClient.connect(device_id.c_str()))
+    {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      mqttClient.publish("outTopic", "hello world");
+      // ... and resubscribe
+      mqttClient.subscribe("inTopic");
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void publish_float(int client, char *topic, float val)
+{
+  char val_buf[10];
+  char topic_buf[20];
+
+  sprintf(topic_buf, "%d/%s/", client, topic);
+  sprintf(val_buf, "%.1f", val);
+
+  mqttClient.publish(topic_buf, val_buf);
+}
+
+void publish_int(int client, char *topic, int val)
+{
+  char val_buf[10];
+  char topic_buf[20];
+
+  sprintf(topic_buf, "%d/%s/", client, topic);
+  sprintf(val_buf, "%d", val);
+
+  mqttClient.publish(topic_buf, val_buf);
+}
+
+/***********************************************************************************************/
---
+++ src/secrets.h
@@ -0,0 +1,6 @@
+/** secrets.h
+ *  Confidential data - DON'T COMMIT THIS
+ */
+
+#define SSID "Kingswood"
+#define PASSWD "wbtc0rar"
